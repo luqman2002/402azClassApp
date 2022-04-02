e4eff6e4fba0a2d731d017be31f5fb1c54b96913
luqman2002 <74608316+luqman2002@users.noreply.github.com>
2022-04-02 14:38:21 +0000

    tv show

--- 402azclassapp/src/components/TvShow.js
+++ 402azclassapp/src/components/TvShow.js
@@ -1,4 +1,7 @@
 import React, { Component, Fragment } from "react";
+import SnowFall from './snow fall wallpaper.jpg';
+import Flash from './the flash wallpaper.jpg';
+import Stranger from './147-1475821_stranger-things-season-3-poster-characters-8k-stranger.jpg';
 
 export default class TvShow extends Component{
     render(){
@@ -6,11 +9,48 @@ export default class TvShow extends Component{
             <section class="hero is-large is-info">
   <div class="hero-body">
     <p class="title">
-      Large hero
+      Tv Shows
     </p>
-    <p class="subtitle">
-      Large subtitle
-    </p>
+
+
+    <div class="columns">
+  <div class="column">
+    Snowfall
+  </div>
+  <div class="column">
+    Second column
+  </div>
+  <div class="column">
+    Third column
+  </div>
+  <div class="column">
+    Fourth column
+  </div>
+</div>
+
+
+
+    <div class="columns">
+  <div class="column">
+  <figure class="image is-128x128">
+  <img src={SnowFall}/>
+</figure>
+  </div>
+  <div class="column">
+  <figure class="image is-128x128">
+  <img src={Flash}/>
+</figure>
+  </div>
+  <div class="column">
+  <figure class="image is-128x128">
+  <img src={Stranger}/>
+</figure>
+  </div>
+  <div class="column">
+    Fourth column
+  </div>
+</div>
+
   </div>
 </section>
         );
