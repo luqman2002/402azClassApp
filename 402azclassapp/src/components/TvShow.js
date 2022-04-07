8f22fe2093d8b3aa8b27c20a72d43512c79b8d1b
luqman2002 <74608316+luqman2002@users.noreply.github.com>
2022-04-07 11:43:50 +0000

    fetch

--- 402azclassapp/src/components/TvShow.js
+++ 402azclassapp/src/components/TvShow.js
@@ -1,12 +1,33 @@
-import React, { Component, Fragment } from "react";
+import React, { Component, useState, Fragment, useEffect } from "react";
 import SnowFall from './snow fall wallpaper.jpg';
 import Flash from './the flash wallpaper.jpg';
 import Stranger from './147-1475821_stranger-things-season-3-poster-characters-8k-stranger.jpg';
 import Wallpaper from './tv show wallpaper.jpg';
 import Orange from './tv shows images/oranhge is the new black.jpg';
 
-export default class TvShow extends Component{
-    render(){
+
+function TvShow(){
+
+
+const [Tvshows, setTvshows] = useState([]);
+
+const baseUrl = "https://847pz3ny24.execute-api.us-east-1.amazonaws.com/TvShows";
+
+const getTvShows = () => {
+  fetch(baseUrl)
+  .then((res) => res.json())
+  .then((res) => {
+    console.log(res.Items)
+    setTvshows(res.Items)
+  })
+
+} 
+
+useEffect (() => {
+  getTvShows()
+}, [])
+
+
         return(
             <section class="hero is-large is-info">
 
@@ -103,6 +124,11 @@ export default class TvShow extends Component{
 
   </div>
 </section>
-        );
-    }
+{Tvshows.map((item, i)=> {
+  return <h1 className='title' key={i}>{item.name}</h1>
+})}
+
+        )
 }    
+
+export default TvShow;
